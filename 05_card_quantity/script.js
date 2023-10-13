d8748bce8d977b761144254a4caee8a0ed98a8c1
Henna Maria <hennamaria2001@gmail.com>
2023-10-13 14:11:01 +0530

    card modified

--- 05_card_quantity/script.js
+++ 05_card_quantity/script.js
@@ -1,22 +1,28 @@
 let cardQuantity = 0;
 document.getElementById("card-btn").addEventListener("click", function (event) {
   event.preventDefault();
-  console.log(`Card Quantity: ${cardQuantity += 1}`);
+  card(1);
 });
 document.getElementById("two-btn").addEventListener("click", function (event) {
   event.preventDefault();
-  console.log(`Card Quantity: ${cardQuantity += 2}`);
+  card(2);
 });
 document.getElementById("three-btn").addEventListener("click", function (event) {
   event.preventDefault();
-  console.log(`Card Quantity: ${cardQuantity += 3}`);
+  card(3);
 });
 document.getElementById("reset-btn").addEventListener("click", function (event) {
   event.preventDefault();
-  console.log(`Card is reset\n card Quantity: ${(cardQuantity = 0)}`);
+  cardQuantity = 0;
+  console.log(`Card is reset\n card Quantity: ${cardQuantity}`);
 });
 
 document.getElementById("show-btn").addEventListener("click", function (event) {
   event.preventDefault();
   console.log(`Card Quantity: ${cardQuantity}`);
 });
+
+function card(value) {
+  cardQuantity += value;
+  console.log(`Card Quantity: ${cardQuantity}`);
+}
