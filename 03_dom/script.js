c4e9316f16e6a6a591f11daa741f35061b28673b
Henna Maria <hennamaria2001@gmail.com>
2023-10-14 14:25:03 +0530

    DOM

--- 03_dom/script.js
+++ 03_dom/script.js
@@ -1,4 +1,6 @@
 const subscribeBtn = document.getElementById("subscribe-btn");
+const inputEl = document.getElementById('input-el');
+const calculateBtn = document.getElementById('calculate-btn');
 subscribeBtn.addEventListener("click", (e) => {
     e.preventDefault();
     if(subscribeBtn.innerText === 'Subscribe'){
@@ -6,5 +8,27 @@ subscribeBtn.addEventListener("click", (e) => {
     } else {
         subscribeBtn.innerHTML = "Subscribe";
     }
-    
   });
+
+  calculateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetchContent();
+  });
+  
+  inputEl.addEventListener('keypress', (e) => {
+   if(e.key === 'Enter'){
+    fetchContent();
+   }
+  });
+
+  const fetchContent = () => {
+    if (parseInt(inputEl.value) < 40) {
+      document.getElementById("cost-el").innerHTML = `$${
+        parseInt(inputEl.value) + 10
+      }`;
+    } else {
+      document.getElementById("cost-el").innerHTML = `$${parseInt(
+        inputEl.value
+      )}`;
+    }
+  }
